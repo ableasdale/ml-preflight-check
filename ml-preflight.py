876a35846e27aa6ff34682b1d4abab206fb5d322
Alex Bleasdale <ableasdale@marklogic.com>
2015-08-29 18:30:59 +0100

    Organising code

--- ml-preflight.py
+++ ml-preflight.py
@@ -1,12 +1,33 @@
 import socket
 import xml.etree.ElementTree as ET
 
+###################################################################
+# Global variables
+###################################################################
+
+MARKLOGIC_FOREST_DIRECTORY = "/var/opt/MarkLogic"
 MARKLOGIC_FOREST_ASSIGNMENTS_XML = "/var/opt/MarkLogic/assignments.xml"
 XML_NAMESPACES = {'a': 'http://marklogic.com/xdmp/assignments'} 
 MARKLOGIC_PORTS = [7997, 7998, 7999, 8000, 8001, 8002]
 LOCALHOST = "0.0.0.0"
 
-def DoesServiceExist(host, port):
+###################################################################
+# Functions 
+###################################################################
+
+"""Check a given TCP port to ensure it's not already bound
+
+    Args:
+        hostname: the hostname (FQDN) or IP address.
+	port: the TCP port.
+
+    Returns:
+        true if the port is already bound.
+
+    pydoc -w checkPortBinding
+
+"""
+def checkPortBinding(hostname, port):
     print("Checking port "+ str(port));
     captive_dns_addr = ""
     host_addr = ""
@@ -17,14 +38,14 @@ def DoesServiceExist(host, port):
         pass
 
     try:
-        host_addr = socket.gethostbyname(host)
+        host_addr = socket.gethostbyname(hostname)
 
         if (captive_dns_addr == host_addr):
             return False
 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(1)
-        s.connect((host, port))
+        s.connect((hostname, port))
         s.close()
     except:
         return False
@@ -32,13 +53,15 @@ def DoesServiceExist(host, port):
     return True
 
 
-# check ports
+###################################################################
+# Main
+###################################################################
+
+# Check ports
 for x in MARKLOGIC_PORTS:
-    print(DoesServiceExist(LOCALHOST, x)),
+    print(checkPortBinding(LOCALHOST, x)),
 
-
-
-# check journals
+# Check journals
 print "Checking Journals for this host [TODO - host]\n"
 tree = ET.parse(MARKLOGIC_FOREST_ASSIGNMENTS_XML)
 root = tree.getroot()
